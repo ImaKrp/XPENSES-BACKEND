8b0a673aafd7f0ca8094ce47028a97b2af16b90b
ImaKrp <julio.c04@aluno.ifsc.edu.br>
2021-11-27 11:49:48 -0300

    removed socket

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 
-import { Server } from "socket.io";
-
 import { router } from "./routes";
 
 const app = express();
@@ -12,18 +10,8 @@ app.use(cors());
 
 const serverHttp = http.createServer(app);
 
-const io = new Server(serverHttp, {
-  cors: {
-    origin: "*",
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`Usuário conectado no socket ${socket.id}`);
-});
-
 app.use(express.json());
 
 app.use(router);
 
-export { serverHttp, io };
+export { serverHttp };
